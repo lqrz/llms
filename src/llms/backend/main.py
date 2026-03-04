4633cd05edf489013b9dc7cea6df4d0915c0acd2
lqrz <lautaro.quiroz@gmail.com>
2026-03-04 16:29:52 -0300

    embeddings model init on lifespan

--- src/llms/backend/main.py
+++ src/llms/backend/main.py
@@ -5,6 +5,8 @@ from collections import defaultdict
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
+import os
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
 from llms.commons.logger import logger
 from llms.backend.routes.agent import router as agent_router
@@ -14,13 +16,20 @@ from llms.backend.routes.server import router as health_router
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     try:
-        # app.state.graph = build_graph()
+
         logger.info("Building basic graph inside lifespan.")
         app.state.thread_locks = defaultdict(asyncio.Lock)
+
+        embeddings_model_name: str = os.getenv("EMBEDDINGS_MODEL")
+        logger.info(f"Instantiating embeddings model {embeddings_model_name}")
+        embeddings_model = HuggingFaceEmbedding(model_name=embeddings_model_name)
+        app.state.embeddings_model = embeddings_model
+
         yield
     except Exception as e:
         logger.error(f"Could not build graph: {e}")
         raise e
+
     finally:
         app.state.graph = None
 
